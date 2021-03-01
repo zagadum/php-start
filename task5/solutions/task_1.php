62d74d4a119d192b71b88a55709d7dac1271dfc1
Ivan <franklin19992001@gmail.com>
2021-03-01 15:21:53 +0200

    задача 1

---
+++ task5/solutions/task_1.php
@@ -0,0 +1,27 @@
+<?php
+
+for ($i = 1; $i <= 25; $i++) {
+    $sum += $i;
+}
+
+echo $sum;
+
+
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+
+$suma=0;
+$x=1;
+do
+{
+    $suma += $x;
+    $x++;
+}
+while($x <= 25);
+
+echo $suma;
+
+?>
+
+
