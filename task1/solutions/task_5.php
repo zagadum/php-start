940fee15c71fea25e3c318de2940939d0d6d65d4
Ivan <franklin19992001@gmail.com>
2021-03-01 12:37:29 +0200

    задача 5

---
+++ task1/solutions/task_5.php
@@ -0,0 +1,23 @@
+<?php
+
+define("FORTYONE", 41);
+define("THIRTYTHREE", 33);
+
+$sum= FORTYONE+THIRTYTHREE;
+echo $sum;
+
+echo '<br/>';
+echo '<br/>';
+echo '<hr/>';
+
+// второй способ
+
+
+const FIRST=41;
+const SECOND=33;
+
+$result=FIRST+SECOND;
+
+echo $result;
+
+?>
