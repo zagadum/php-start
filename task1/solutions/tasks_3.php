1cc9255c3b0450af33830e84b0d682fd18320c2f
Ivan <franklin19992001@gmail.com>
2021-03-01 11:36:40 +0200

    задача 3

---
+++ task1/solutions/tasks_3.php
@@ -0,0 +1,21 @@
+<?php
+
+$three=3;
+$five=5;
+$eight=8;
+
+echo $three,',', $five,',', $eight=8;
+
+echo '<br/>';
+echo '<hr/>';
+
+$sum=$three+$five+$eight;
+echo $sum;
+
+echo '<br/>';
+echo '<hr/>';
+
+$result= 2+6+2/5-1;
+echo $result;
+
+?>
