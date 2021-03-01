cc7a3d4a3b50880089bf56fd61fe44f6eec88107
Ivan <franklin19992001@gmail.com>
2021-03-01 13:00:20 +0200

    задача 1

---
+++ task2/task_1.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = 152; /*тип integer*/
+$b = '152';/*тип string*/
+$c = 'London';/*тип string*/
+$d = array(152);/*тип array*/
+$e = 15.2;/*тип double*/
+$f = false;/*тип boolean*/
+$g = true;/*тип boolean*/
+
+echo $a;
+echo '<br/>';
+echo $b;
+echo '<br/>';
+echo $c;
+echo '<br/>';
+echo $d;
+echo '<br/>';
+echo $e;
+echo '<br/>';
+echo $f;
+echo '<br/>';
+echo $g;
+
+
+?>
