699c4be0e410f52bb934ac7f7b30ff50d93ed1a1
zagadum <zagadum@ukr.net>
2021-03-02 17:43:30 +0200

    fix

--- task7/solutions/task_1.php
+++ task7/solutions/task_1.php
@@ -1,29 +1,23 @@
 <?php
 
-
-
-
-//$price=0;
-//$quantity=0;
+ 
 function sum(){
+    $price=0;
+    $quantity=0;
+	foreach ( $products as $product){
+		$price+=$product['price']*$product['quantity'];
+		$quantity+=$product['quantity'];
+	}
 
+}
 
-    $products = array(
+  $products = array(
         array('name' => 'Телевизор', 'price' => '400', 'quantity' => 1),
         array('name' => 'Телефон', 'price' => '300', 'quantity' => 3),
         array('name' => 'Кроссовки', 'price' => '150', 'quantity' => 2),
     );
 
-
-
-
-    $price=0;
-    $quantity=0;
-foreach ( $products as $product){
-    $price+=$product['price']*$product['quantity'];
-    $quantity+=$product['quantity'];
-
-}
+echo sum();
 
 echo 'Общая сумма покупок - '.$price;
 
@@ -31,9 +25,6 @@ echo '<br/>';
 echo '<br/>';
 
 echo 'Общее количество выбранных товаров - '.$quantity;
-}
 
-
-echo sum();
 
 ?>
