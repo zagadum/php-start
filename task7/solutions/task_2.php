0d5aad2ad34c474bedb4dda57837b6e10b3f4790
Ivan <franklin19992001@gmail.com>
2021-03-02 15:14:21 +0200

    задача 2

--- task7/solutions/task_2.php
+++ task7/solutions/task_2.php
@@ -1,15 +1,15 @@
 <?php
 
 
-function sqrFunction($a,$b,$c = 0)
+function sqrFunction($a,$b,$c)
 {
 
     $d = ($b * $b) - (4 * $a * $c);
 
     if ($d > 0) {
 
-        $x1 = ((-$b) - sqrt($d)) / 2 * $a;
-        $x2 = ((-$b) + sqrt($d)) / 2 * $a;
+        $x1 = ((-$b) - sqrt($d)) / (2 * $a);
+        $x2 = ((-$b) + sqrt($d)) / (2 * $a);
         return array('x1' => $x1, 'x2' => $x2);
     } elseif ($d == 0) {
         return (-$b + sqrt($d)) / (2 * $a);
@@ -17,11 +17,6 @@ function sqrFunction($a,$b,$c = 0)
         return false;
     }
 }
-print_r(sqrFunction(1, -8, 7));
-
-
-
-
-
+print_r(sqrFunction(3, 9, -54));
 
 ?>
