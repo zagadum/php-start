2a76c8e30dc99f250a8888a4ac16f2eaa31d5f3c
Ivan <franklin19992001@gmail.com>
2021-03-02 11:51:51 +0200

    задача 1 *исправил*

--- task7/solutions/task_1.php
+++ task7/solutions/task_1.php
@@ -1,23 +1,39 @@
 <?php
 
-function start(){
+
+
+
+//$price=0;
+//$quantity=0;
+function sum(){
+
+
     $products = array(
         array('name' => 'Телевизор', 'price' => '400', 'quantity' => 1),
         array('name' => 'Телефон', 'price' => '300', 'quantity' => 3),
         array('name' => 'Кроссовки', 'price' => '150', 'quantity' => 2),
     );
 
-    $price=$products[0]['price']+$products[1]['price']+$products[2]['price'];
-    $quantity=$products[0]['quantity']+$products[1]['quantity']+$products[2]['quantity'];
-    echo $price." -общая цена";
-    echo '<br/>';
-    echo '<br/>';
-    echo $quantity." -общее количество";
+
+
+
+    $price=0;
+    $quantity=0;
+foreach ( $products as $product){
+    $price+=$product['price']*$product['quantity'];
+    $quantity+=$product['quantity'];
+
+}
+
+echo 'Общая сумма покупок - '.$price;
+
+echo '<br/>';
+echo '<br/>';
+
+echo 'Общее количество выбранных товаров - '.$quantity;
 }
 
 
-
-
-echo start();
+echo sum();
 
 ?>
