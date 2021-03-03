de54ea28e3555b729743bbfbb91fb56285e340df
Ivan <franklin19992001@gmil.com>
2021-03-03 19:33:32 +0200

    задача 2 Добавил кнопку

--- task9/solutions/task_2.php
+++ task9/solutions/task_2.php
@@ -3,8 +3,28 @@ $counter = $_COOKIE["counter"];
 if (!isset($counter)){
 $counter = date('Y-m-d H:i:s');
 }
-else{
-    $counter = $counter;
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="task_2.php">
+
+        <input type="submit" name="submit" value="Узнать время">
+
+    </form>
+</body>
+</html>
+
+<?php
+if(isset($_POST['submit'])){
+    echo $counter;
 }
-echo $counter;
 ?>
