5b102771d724889789da8982fc4c4eda76b6feff
Ivan <franklin19992001@gmail.com>
2021-03-19 00:36:48 +0200

    сделал массив наискосяк и массив реверсивно

--- learn-tesing/task_array.php
+++ learn-tesing/task_array.php
@@ -30,43 +30,60 @@ for($i=0; $i<$n; $i++){
 for($i=0; $i<count($arrayEmpty); $i++){
 
     for ($j=0; $j<count($arrayEmpty[$i]);$j++){
-        echo $arrayEmpty[$i][$j].' ';
+        if ($arrayEmpty[$i][$j]==1){
+            echo "<span style='color:red;'>".$arrayEmpty[$i][$j].' '."</span>";
+        }else{
+            echo "<span >".$arrayEmpty[$i][$j].' '."</span>";
+        }
     }
     echo "<br/>";
 }
 
 
-//echo "<br/>";
-//echo "<br/>";
-//echo "<br/>";
-//echo "<br/>";
-//
-//
-//$m=5;
-//$arrayReverse=[];
-//
-//for($x=0; $x<$m; $x++){
-//    $arrayReverse[]=[];
-//    for ($y=0; $y<$m;$y++){
-//
-//        if ($x==$y){
-//            $arrayReverse[$x][]=1;
-//
-//        } else{
-//            $arrayReverse[$x][]=0;
-//
-//        }
-//
-//    }
-//
-//}
-//for($x=0; $x<count($arrayReverse); $x++){
-//
-//    for ($y=0; $y<count($arrayReverse[$x]);$y++){
-//        echo $arrayReverse[$x][$y].' ';
-//    }
-//    echo "<br/>";
-//}
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+
+
+
+
+
+
+
+
+$m=10;
+$mk=$m;
+$arrayReverse=[];
+
+for($x=0; $x<$m; $x++){
+    $arrayReverse[]=[];
+    for ($y=0; $y<$m;$y++){
+
+        if ($mk-$x==$y+1){
+            $arrayReverse[$x][]=1;
+
+        } else{
+            $arrayReverse[$x][]=0;
+
+        }
+
+    }
+
+}
+for($x=0; $x<count($arrayReverse); $x++){
+
+    for ($y=0; $y<count($arrayReverse[$x]);$y++){
+
+        if ($arrayReverse[$x][$y]==1){
+            echo "<span style='color:red;'>".$arrayReverse[$x][$y].' '."</span>";
+        }else{
+            echo "<span >".$arrayReverse[$x][$y].' '."</span>";
+        }
+    }
+    echo "<br/>";
+}
 
 
 
